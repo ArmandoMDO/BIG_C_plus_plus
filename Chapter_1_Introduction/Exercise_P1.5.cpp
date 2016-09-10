f2fb9312da72a927ceaed56f64a2aa29b58f8f78
ArmandoMDO <ArmandoPC.Tech@Gmail.com>
2016-09-10 15:59:31 -0700

    Exercise_P1.5.cpp

---
+++ Chapter_1_Introduction/Exercise_P1.5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << endl << endl;
+  cout << "+---------+" << endl;
+  cout << "| Armando |" << endl;
+  cout << "+---------+" << endl;
+  return 0;
+}
