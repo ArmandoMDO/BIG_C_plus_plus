f3cf908868a2f3972a0ae11207a267befb482353
ArmandoMDO <ArmandoPC.Tech@Gmail.com>
2016-09-10 17:24:21 -0700

    Create Exercise_P2.1.cpp

---
+++ Chapter_2_Numbers_and_Objects/Exercise_P2.1.cpp
@@ -0,0 +1,132 @@
+//Armando Montes de Oca
+//09-10-2016
+//Exercise P2.1
+//Version 1.0
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "int" << endl;
+	int 
+		a1=1, 
+		a2=10, 
+		a3=100, 
+		a4=1000, 
+		a5=10000, 
+		a6=100000, 
+		a7=1000000, 
+		a8=10000000, 
+		a9=100000000, 
+	 a10=1000000000, 
+	 a11=10000000000, 
+	 a12=100000000000;
+	cout << a1 << endl << a2 << endl << a3 << endl << a4 << endl << a5 << endl << a6 << endl 
+	<< a7 << endl << a8 << endl << a9 << endl << a10 << endl << a11 << endl << a12 << endl;
+
+	cout << endl << "unsigned" << endl;
+	unsigned 	
+		b1=1, 
+		b2=10, 
+		b3=100, 
+		b4=1000, 
+		b5=10000, 
+		b6=100000, 
+		b7=1000000, 
+		b8=10000000, 
+		b9=100000000, 
+	 b10=1000000000, 
+	 b11=10000000000, 
+	 b12=100000000000;
+	cout << b1 << endl << b2 << endl << b3 << endl << b4 << endl << b5 << endl << b6 << endl 
+	<< b7 << endl << b8 << endl << b9 << endl << b10 << endl << b11 << endl << b12 << endl;
+	
+	cout << endl << "short" << endl;
+	short 
+		c1=1, 
+		c2=10, 
+		c3=100, 
+		c4=1000, 
+		c5=10000, 
+		c6=100000, 
+		c7=1000000, 
+		c8=10000000, 
+		c9=100000000, 
+	 c10=1000000000, 
+	 c11=10000000000, 
+	 c12=100000000000;
+	cout << c1 << endl << c2 << endl << c3 << endl << c4 << endl << c5 << endl << c6 << endl 
+	<< c7 << endl << c8 << endl << c9 << endl << c10 << endl << c11 << endl << c12 << endl;
+
+	cout << endl << "unsigned short" << endl;
+	unsigned short 
+		d1=1, 
+		d2=10, 
+		d3=100, 
+		d4=1000, 
+		d5=10000, 
+		d6=100000, 
+		d7=1000000, 
+		d8=10000000, 
+		d9=100000000, 
+	 d10=1000000000, 
+	 d11=10000000000, 
+	 d12=100000000000;
+	cout << d1 << endl << d2 << endl << d3 << endl << d4 << endl << d5 << endl << d6 << endl 
+	<< d7 << endl << d8 << endl << d9 << endl << d10 << endl << d11 << endl << d12 << endl;
+	
+	cout << endl << "long long" << endl;
+	long long 
+		e1=1, 
+		e2=10, 
+		e3=100, 
+		e4=1000, 
+		e5=10000, 
+		e6=100000, 
+		e7=1000000, 
+		e8=10000000, 
+		e9=100000000, 
+	 e10=1000000000, 
+	 e11=10000000000, 
+	 e12=100000000000;
+	cout << e1 << endl << e2 << endl << e3 << endl << e4 << endl << e5 << endl << e6 << endl 
+	<< e7 << endl << e8 << endl << e9 << endl << e10 << endl << e11 << endl << e12 << endl;
+	
+	cout << endl << "double" << endl;
+	double 
+		f1=1, 
+		f2=10, 
+		f3=100, 
+		f4=1000, 
+		f5=10000, 
+		f6=100000, 
+		f7=1000000, 
+		f8=10000000, 
+		f9=100000000, 
+	 f10=1000000000, 
+	 f11=10000000000, 
+	 f12=100000000000;
+	cout << f1 << endl << f2 << endl << f3 << endl << f4 << endl << f5 << endl << f6 << endl 
+	<< f7 << endl << f8 << endl << f9 << endl << f10 << endl << f11 << endl << f12 << endl;
+	
+	cout << endl << "float" << endl;
+	float 
+		g1=1, 
+		g2=10, 
+		g3=100, 
+		g4=1000, 
+		g5=10000, 
+		g6=100000, 
+		g7=1000000, 
+		g8=10000000, 
+		g9=100000000, 
+	 g10=1000000000, 
+	 g11=10000000000, 
+	 g12=100000000000;
+	cout << g1 << endl << g2 << endl << g3 << endl << g4 << endl << g5 << endl << g6 << endl 
+	<< g7 << endl << g8 << endl << g9 << endl << g10 << endl << g11 << endl << g12 << endl;
+
+	return 0;
+}
